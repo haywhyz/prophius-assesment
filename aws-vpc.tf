88520c94b9629db3154550eea13ab456a229f926
IDOWU OLAYINKA AYOBAMI <olayinkancs@gmail.com>
2023-07-26 19:38:04 +0100

    Create aws-vpc.tf

---
+++ aws-vpc.tf
@@ -0,0 +1,24 @@
+ // VPC MODULE 
+ 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+
+  name = var.vpc_name
+
+  cidr = "10.0.0.0/16"
+  azs  = var.azs 
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+ 
+}
