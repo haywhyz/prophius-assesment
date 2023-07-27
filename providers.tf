5503b749d135648b7c3774767fd52e42714e4192
IDOWU OLAYINKA AYOBAMI <olayinkancs@gmail.com>
2023-07-27 11:36:05 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,33 +1,85 @@
-terraform {
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-    }
-    local = {
-      source = "hashicorp/local"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.57.0"
-    }
+#CLUSTER VARIABLES
 
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.10.0"
-    }
+// GENERAL VARIBALES
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "aws_access_key_id" {
+
+}
+
+variable "aws_secret_access_key" {
+  
+}
+
+// ECR VARIABLES
+variable "repository_name" {
+  default = "prophius-repo"
+}
+
+
+// VPC VARIABLES
+variable "vpc_subnet" {
+  default = "172.16.0.0/16"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["subnet-29cccf61", "subnet-c199a9a7", "subnet-7ab8c520"]
+}
+
+variable "tags" {
+  type = map(string)
+
+  default = {
+    "Environment" = "Development"
   }
 }
 
 
-
-
-
-provider "aws" {
-  region = var.region
+// EKS VARIABLES
+variable "cluster_name" {
+  type    = string
+  default = "prophius-cluster"
 }
 
-provider "local" {
+
+//RDS VARIABLES
+variable "rds_name" {
+  default = "prophiusdb"
 }
 
-provider "null" {
+variable "rds_port" {
+  default = "3306"
+}
+
+variable "rds_identifer" {
+  default = "prophius_db"
+}
+
+
+variable "rds_instance_class" {
+  default = "db.t3.micro"
+}
+
+variable "rds_username" {
+  default = "admin"
+}
+
+variable "rds_password" {
+  default = "P@ssword"
+}
+
+variable "enable_dashboard" {
+  default = true
+}
+
+variable "vpc_name" {
+  default = "pro_vpc"
 }
